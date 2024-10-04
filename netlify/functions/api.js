3dc199251da9d3ed8659a9326f489323e9e6d018
Nafi <nafiyadargaw360@gmail.com>
2024-10-04 18:04:03 +0300

    a

---
+++ netlify/functions/api.js
@@ -0,0 +1,9 @@
+const { createServer, Proxy } = require('aws-serverless-express'); // Import AWS serverless express
+
+const app = require('../dist/src/main.js'); // Import your NestJS application
+
+const server = createServer(app);
+
+exports.handler = (event, context) => {
+  return Proxy(server, event, context); // Proxy the request to your NestJS app
+};
